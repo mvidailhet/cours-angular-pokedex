050fcdf4c6ba4cca74aa6562cc28aa63ac2e12f0
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-04-15 12:14:15 +0200

    @Input, pour chaque pokemon de 'pokemon-list' passage de sa propriété 'name' à 'pokemon-item'

--- src/app/components/pokemon-item/pokemon-item.component.ts
+++ src/app/components/pokemon-item/pokemon-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-item',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonItemComponent implements OnInit {
   level = Math.round(Math.random() * 100);
+  @Input() name = '';
 
   constructor() {}
 
