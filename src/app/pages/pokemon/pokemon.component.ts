60b305ad339b8463740f827c31d8e4c3fca0de37
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-04-21 14:13:26 +0200

    Récupération des query params et du fragment

--- src/app/pages/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -13,6 +14,8 @@ export class PokemonComponent implements OnInit, OnDestroy {
   previousPokemonName: string | undefined;
   nextPokemonName: string | undefined;
   paramsSubscription: Subscription | undefined;
+  queryParamsSubscription: Subscription | undefined;
+  fragmentSubscription: Subscription | undefined;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -21,17 +24,9 @@ export class PokemonComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.paramsSubscription = this.activatedRoute.params.subscribe((params: Params) => {
-      const pokemonName = params.name;
-      const pokemonIndex = this.pokemonService.findPokemonIndexByName(pokemonName);
-      this.nextPokemonName = this.pokemonService.getNextPokemonName(pokemonName);
-      this.previousPokemonName = this.pokemonService.getPreviousPokemonName(pokemonName);
-      this.pokemon = {
-        id: this.pokemonService.pokemons[pokemonIndex].id,
-        name: pokemonName,
-        level: this.pokemonService.pokemons[pokemonIndex].level,
-      };
-    });
+    this.paramsSubscription = this.activatedRoute.params.subscribe(this.handleRouteParams);
+    this.queryParamsSubscription = this.activatedRoute.queryParams.subscribe(this.handleQueryParams);
+    this.fragmentSubscription = this.activatedRoute.fragment.subscribe(this.handleFragment);
   }
 
   ngOnDestroy(): void {
@@ -39,6 +34,26 @@ export class PokemonComponent implements OnInit, OnDestroy {
     // Mais pour l'exemple, on montre comment faire
     // Si cela n'est pas fait, la subscription reste dans la mémoire et crée une fuite mémoire
     this.paramsSubscription?.unsubscribe();
+  }
+
+  handleRouteParams = (params: Params) => {
+    const pokemonName = params.name;
+    const pokemonIndex = this.pokemonService.findPokemonIndexByName(pokemonName);
+    this.nextPokemonName = this.pokemonService.getNextPokemonName(pokemonName);
+    this.previousPokemonName = this.pokemonService.getPreviousPokemonName(pokemonName);
+    this.pokemon = {
+      id: this.pokemonService.pokemons[pokemonIndex].id,
+      name: pokemonName,
+      level: this.pokemonService.pokemons[pokemonIndex].level,
+    };
+  };
+
+  handleQueryParams(queryParams: Params) {
+    console.log('query parameters :', queryParams);
+  }
+
+  handleFragment(fragment: string | null) {
+    console.log(`fragment : ${fragment}`);
   }
 
   goToPreviousPokemon() {
