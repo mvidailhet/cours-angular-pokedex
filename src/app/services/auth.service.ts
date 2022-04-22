ffe8389ed9826e07e07ac6a502ba0751920bdf4c
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-04-22 14:18:34 +0200

    Redirection vers l'accueille si on se déconnecte depuis la page 'Mes Pokémon'

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   loggedIn = false;
+  constructor(private router: Router) {}
 
   login() {
     this.loggedIn = true;
   }
 
   logout() {
+    const isPokemonPage = /^\/pokemon\//.test(this.router.url);
     this.loggedIn = false;
+    if (isPokemonPage) {
+      this.router.navigate(['/']);
+    }
   }
 
   isAuthenticated(): Promise<boolean> {
