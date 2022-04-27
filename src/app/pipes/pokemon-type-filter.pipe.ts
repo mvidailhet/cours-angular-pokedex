c8aba849ffb1eef926a349917abea44cca9a0a0d
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-04-27 11:22:54 +0200

    Changement du pipe de filtre en 'impur' pour refiltrer la liste de pokémon lors de ajout d'un pokemon

--- src/app/pipes/pokemon-type-filter.pipe.ts
+++ src/app/pipes/pokemon-type-filter.pipe.ts
@@ -3,6 +3,7 @@ import { Pokemon, PokemonTypeEnum } from '../services/pokemons.service';
 
 @Pipe({
   name: 'pokemonTypeFilter',
+  pure: false,
 })
 export class PokemonTypeFilterPipe implements PipeTransform {
   transform(pokemons: Pokemon[], type: PokemonTypeEnum | undefined | string): Pokemon[] {
