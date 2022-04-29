834b687cc66ef556ef1b7b28ee2eec04cf68fe10
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-04-29 09:14:37 +0200

    Création du formulaire dans le code TypeScript

--- src/app/pages/create-user-model-driven/create-user-model-driven.component.ts
+++ src/app/pages/create-user-model-driven/create-user-model-driven.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user-model-driven',
   templateUrl: './create-user-model-driven.component.html',
   styleUrls: ['./create-user-model-driven.component.scss'],
 })
-export class CreateUserModelDrivenComponent {}
+export class CreateUserModelDrivenComponent {
+  userForm: FormGroup;
+
+  constructor() {
+    this.userForm = new FormGroup({
+      username: new FormControl('Mitch'),
+      email: new FormControl(),
+    });
+  }
+}
