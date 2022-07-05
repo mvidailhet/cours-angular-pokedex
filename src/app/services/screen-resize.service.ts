a59d31caaa92c9331bd9931111eeaf7305333f9f
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-07-05 15:15:55 +0200

    Création d'une observable pour détecter le changement de breakpoint lors du resize

--- src/app/services/screen-resize.service.ts
+++ src/app/services/screen-resize.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { fromEvent, map, sampleTime, startWith } from 'rxjs';
+import { EMPTY, fromEvent, map, of, sampleTime, startWith, switchMap } from 'rxjs';
 
 export enum SCREEN_SIZE_BREAKPOINTS {
   XS = 0,
@@ -14,6 +14,9 @@ export enum SCREEN_SIZE_BREAKPOINTS {
   providedIn: 'root',
 })
 export class ScreenResizeService implements OnDestroy {
+  private _currentScreenWidth: number | undefined;
+  private _currentBreakpoint: SCREEN_SIZE_BREAKPOINTS | undefined | null;
+
   onResize$ = fromEvent(window, 'resize').pipe(
     sampleTime(300),
     map((event: Event) => {
@@ -23,9 +26,49 @@ export class ScreenResizeService implements OnDestroy {
     startWith(window.innerWidth),
   );
 
-  onResizeSubscription = this.onResize$.subscribe((newWidth: number) => {
-    console.log(newWidth);
-  });
+  onScreenSizeBreakpointChange$ = this.onResize$.pipe(
+    switchMap((newScreenWidth: number) => {
+      const newBreakpoint = this.detectScreenSizeBreakpointChange(newScreenWidth);
+      if (!newBreakpoint) return EMPTY;
+      return of(newBreakpoint);
+    }),
+  );
+
+  onResizeSubscription = this.onScreenSizeBreakpointChange$.subscribe(
+    (newBreakpoint: SCREEN_SIZE_BREAKPOINTS | null) => {
+      this._currentBreakpoint = newBreakpoint;
+    },
+  );
+
+  private detectScreenSizeBreakpointChange(screenWidth: number): SCREEN_SIZE_BREAKPOINTS | null {
+    if (screenWidth === this._currentScreenWidth) return null;
+    this._currentScreenWidth = screenWidth;
+
+    const newBreakpoint = this.getScreenSizeBreakpoint(screenWidth);
+    if (newBreakpoint === this._currentBreakpoint) return null;
+
+    this._currentBreakpoint = newBreakpoint;
+    return this._currentBreakpoint;
+  }
+
+  private getScreenSizeBreakpoint(screensize: number): SCREEN_SIZE_BREAKPOINTS {
+    if (screensize < SCREEN_SIZE_BREAKPOINTS.SM) {
+      return SCREEN_SIZE_BREAKPOINTS.XS;
+    }
+    if (screensize < SCREEN_SIZE_BREAKPOINTS.MD) {
+      return SCREEN_SIZE_BREAKPOINTS.SM;
+    }
+    if (screensize < SCREEN_SIZE_BREAKPOINTS.LG) {
+      return SCREEN_SIZE_BREAKPOINTS.MD;
+    }
+    if (screensize < SCREEN_SIZE_BREAKPOINTS.XL) {
+      return SCREEN_SIZE_BREAKPOINTS.LG;
+    }
+    if (screensize < SCREEN_SIZE_BREAKPOINTS.XXL) {
+      return SCREEN_SIZE_BREAKPOINTS.XL;
+    }
+    return SCREEN_SIZE_BREAKPOINTS.XXL;
+  }
 
   ngOnDestroy(): void {
     this.onResizeSubscription.unsubscribe();
